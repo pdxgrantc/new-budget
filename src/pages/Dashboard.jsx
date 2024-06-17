41d9204a2a4e576ca68f22e9ebb528e0fb275aa4
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-06-17 14:50:25 -0700

    remove code from dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,39 +1,9 @@
-import SignIn from "../utility/SignIn"
-import SignOut from "../utility/SignOut"
-
-import userSlice from "../../redux/userSlice"
 import { useSelector } from 'react-redux';
 
 export default function Dashboard() {
   return (
     <div className="flex flex-col">
-      <div>
-        <SignIn>
-          Ding Dong MOTHAFUCKA
-        </SignIn>
-      </div>
-      <div>
-        <SignOut>
-          Goodbye MOTHAFUCKA
-        </SignOut>
-      </div>
-      <PrintUserSlice />
+
     </div>
   )
-}
-
-function PrintUserSlice() {
-  // Use useSelector hook to access the state managed by userSlice
-  const userState = useSelector((state) => state.user);
-
-  // Function to log the userSlice state to the console
-  const printUserSliceToConsole = () => {
-    console.log(userState);
-  };
-
-  return (
-    <div>
-      <button onClick={printUserSliceToConsole}>Print To Console</button>
-    </div>
-  );
 }
