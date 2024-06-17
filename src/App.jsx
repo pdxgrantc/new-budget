d958ecfd5278f2d270b39ea740bff8804d9cd8e7
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-06-16 18:05:37 -0700

    rendering dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import PropTypes from "prop-types";
 
 // Pages
