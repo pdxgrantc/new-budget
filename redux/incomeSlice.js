32566fc139976a1bd254b149900dda4574872d14
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-06-21 08:35:16 -0700

    chore: Update assets and fix incomeSlice

--- redux/incomeSlice.js
+++ redux/incomeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { WriteUserDoc } from './utils';
 
 const initialState = {
-    user: null,
+    income: null,
     loading: false,
     error: null,
 };
@@ -12,10 +12,22 @@ const incomeSlice = createSlice({
     name: 'income',
     initialState,
     reducers: {
-
+        setIncome: (state, action) => {
+            state.income = action.payload;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
     },
 });
 
-export const { } = incomeSlice.actions;
+export const {
+    setIncome,
+    setLoading,
+    setError
+} = incomeSlice.actions;
 
 export default incomeSlice.reducer;
